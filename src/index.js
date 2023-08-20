9437675f36155b950ee676a48a628478aced7cf4
VIJAY <vijaydhiman51@gmail.com>
2023-08-21 00:26:23 +0530

    Section 10 - React Context for state management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-import './index.scss';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+
+import './index.scss';
+
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById('root');
@@ -11,7 +15,9 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
